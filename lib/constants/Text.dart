ba6456092ff76b49add7e712b29e1d894469476f
Bartek O <bartek@fos.nl>
2022-01-31 22:13:36 +0100

    Added text for every "tab"

--- lib/constants/Text.dart
+++ lib/constants/Text.dart
@@ -1,5 +1,14 @@
 part of constants;
 
 class Text {
+  static const appTitle = "ThuisGemaakt";
+
+  // navigation bar names
+  static const thisWeekName = "Home";
+  static const favouritesName = "Favorieten";
+  static const exploreName = "Explore";
+  static const accountName = "Account";
+  static const fridgeName = "Koelkast";
+
   static const thisWeekTitle = "Gerechten voor deze week";
 }
