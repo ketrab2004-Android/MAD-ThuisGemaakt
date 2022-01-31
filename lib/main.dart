6b476b621ac11bd900c31740167d5236b05847ce
Bartek O <bartek@fos.nl>
2022-01-31 22:15:41 +0100

    Moved scaffolding to main (every page has the same scaffold)

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:thuis_gemaakt/pages/l.dart' as Pages;
+import 'package:thuis_gemaakt/styles/l.dart' as Styles;
+import 'package:thuis_gemaakt/constants/l.dart' as Constants;
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  Styles.Base theme = Styles.Lightmode();
+
+  List< List > navigationItems = [
+    [Constants.Text.thisWeekName, Icons.home],
+    [Constants.Text.favouritesName, Icons.favorite],
+    [Constants.Text.exploreName, Icons.search],
+    [Constants.Text.accountName, Icons.person],
+    [Constants.Text.fridgeName, Icons.kitchen],
+  ];
+
+  // if (true) {
+  //   navigationItems.add(
+  //     [Constants.Text.fridgeName, Icons.fridge]
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ThuisGemaakt',
+      title: Constants.Text.appTitle,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: theme.themeSwatch,
       ),
-      home: Pages.HomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        body: SafeArea(
+          child: Pages.HomePage(
+            style: theme,
+            title: Constants.Text.appTitle,
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: new List.generate(navigationItems.length, (index) {
+            List item = navigationItems[index];
+            return new BottomNavigationBarItem(
+              icon: Icon(
+                item[1],
+                color: theme.navigationIconColor,
+              ),
+              label: item[0],
+            );
+          }),
+          showUnselectedLabels: true,
+          selectedItemColor: theme.navigationIconSelectedColor,
+        ),
+      ),
     );
   }
 }
