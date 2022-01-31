6229d947044524014c9c7fbca7f7607722556d5c
Bartek O <bartek@fos.nl>
2022-01-31 22:14:40 +0100

    Renamed import of style to Styles
    in widgets

--- lib/widgets/l.dart
+++ lib/widgets/l.dart
@@ -1,6 +1,6 @@
 library widgets;
 
 import 'package:flutter/material.dart';
-import 'package:thuis_gemaakt/styles/l.dart';
+import 'package:thuis_gemaakt/styles/l.dart' as Styles;
 
 part 'H1.dart';
