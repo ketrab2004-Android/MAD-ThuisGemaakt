cc99cc214e27e0dc013ec99045f5f5b5a2daf109
Bartek O <bartek@fos.nl>
2022-01-31 22:13:50 +0100

    Added import of styles for pages

--- lib/pages/l.dart
+++ lib/pages/l.dart
@@ -3,5 +3,6 @@ library pages;
 import 'package:flutter/material.dart';
 import 'package:thuis_gemaakt/widgets/l.dart' as Widgets;
 import 'package:thuis_gemaakt/constants/l.dart' as Constants;
+import 'package:thuis_gemaakt/styles/l.dart' as Styles;
 
 part 'HomePage.dart';
