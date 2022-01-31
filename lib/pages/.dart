61e2ce0f85393eab7ebe24bf0042737cc400755e
Bartek O <bartek@fos.nl>
2022-01-31 19:04:09 +0100

    Made widgets import as Widgets

--- lib/pages/.dart
+++ lib/pages/.dart
@@ -1,6 +1,6 @@
 library pages;
 
 import 'package:flutter/material.dart';
-import 'package:thuis_gemaakt/widgets/.dart';
+import 'package:thuis_gemaakt/widgets/.dart' as Widgets;
 
 part 'HomePage.dart';
