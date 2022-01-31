02f4655a4b70b69bcc89d10c0a842052486bfe2d
Bartek O <bartek@fos.nl>
2022-01-31 19:41:06 +0100

    Created sizes and text constants

---
+++ lib/constants/Sizes.dart
@@ -0,0 +1,6 @@
+part of constants;
+
+class Sizes {
+  static const double weekVSpacing = 20.0;
+  static const double weekHSpacing = 20.0;
+}
---
+++ lib/constants/Text.dart
@@ -0,0 +1,5 @@
+part of constants;
+
+class Text {
+  static const thisWeekTitle = "Gerechten voor deze week";
+}
