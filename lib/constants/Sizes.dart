cb5261efe351fb723ef3fd188a98e6b53f56d4d1
Bartek O <bartek@fos.nl>
2022-01-31 22:13:23 +0100

    Added padding for week page title

--- lib/constants/Sizes.dart
+++ lib/constants/Sizes.dart
@@ -1,8 +1,10 @@
 part of constants;
 
 class Sizes {
-  static const EdgeInsetsGeometry viewPadding = EdgeInsets.all(10.0);
+  static const EdgeInsetsGeometry pagePadding = EdgeInsets.all(10.0);
 
-  static const double weekVSpacing = 20.0;
+  static const EdgeInsetsGeometry weekTitlePadding = EdgeInsets.fromLTRB(2.5, 5.0, 2.5, 10.0);
+  static const EdgeInsetsGeometry weekItemsPadding = EdgeInsets.all(10.0);
+  static const double weekVSpacing = 10.0;
   static const double weekHSpacing = 20.0;
 }
