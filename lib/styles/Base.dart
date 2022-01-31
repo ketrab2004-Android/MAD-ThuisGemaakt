50b954f6d7ae55bf416bf8f688fed5512824e930
Bartek O <bartek@fos.nl>
2022-01-31 20:42:30 +0100

    Added text colours to base

--- lib/styles/Base.dart
+++ lib/styles/Base.dart
@@ -3,5 +3,8 @@ part of styles;
 abstract class Base {
   static const Color theme = Colors.redAccent;
 
+  static const Color text = Colors.black;
+  static const Color disabledText = Colors.black12;
+
   static const Color background = Colors.white;
 }
