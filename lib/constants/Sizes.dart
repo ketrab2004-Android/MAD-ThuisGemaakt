1a2db4695d8cc3004a6ed27f67aef698e853251c
Bartek O <bartek@fos.nl>
2022-01-31 20:43:33 +0100

    Added viewPadding

--- lib/constants/Sizes.dart
+++ lib/constants/Sizes.dart
@@ -1,6 +1,8 @@
 part of constants;
 
 class Sizes {
+  static const EdgeInsetsGeometry viewPadding = EdgeInsets.all(10.0);
+
   static const double weekVSpacing = 20.0;
   static const double weekHSpacing = 20.0;
 }
