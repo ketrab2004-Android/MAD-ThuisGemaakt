44183af940599daacef3c3f0964d82e9609b4cb7
Bartek O <bartek@fos.nl>
2022-02-01 22:53:29 +0100

    Created fridge page

--- lib/pages/Fridge.dart
+++ lib/pages/Fridge.dart
@@ -11,8 +11,77 @@ class Fridge extends StatelessWidget {
       this.style, this.title,
       selectedTab: 4,
       body: ListView(
+        padding: Constants.Sizes.pagePadding,
         children: <Widget>[
+          Widgets.H1(style, Constants.Text.fridgeTitle),
+          Table(
+            border: style.fridgeTableBorder,
+            columnWidths: {
+              0: FlexColumnWidth(5),
+              1: FlexColumnWidth(5),
+              2: FlexColumnWidth(2),
+            },
+            children: <TableRow>[
+              TableRow(
+                children: <Widget>[
+                  Widgets.H1(style, Constants.Text.fridgeIngredient),
+                  Widgets.H1(style, Constants.Text.fridgeAmount),
+                  Widgets.H1(style, ''),
+                ],
+              ),
+              TableRow(
 
+                children: <Widget>[
+                  Widgets.H2(style, "Kaas"),
+                  Widgets.H2(style, "2 plakjes"),
+                  Padding(
+                    padding: Constants.Sizes.fridgeItemPadding,
+                    child: Icon(Icons.edit),
+                  ),
+                ],
+              ),
+              TableRow(
+                children: <Widget>[
+                  Widgets.H2(style, "Melk"),
+                  Widgets.H2(style, "2 liter"),
+                  Padding(
+                    padding: Constants.Sizes.fridgeItemPadding,
+                    child: Icon(Icons.edit),
+                  ),
+                ],
+              ),
+              TableRow(
+                children: <Widget>[
+                  Widgets.H2(style, "Bloem"),
+                  Widgets.H2(style, "200 gram"),
+                  Padding(
+                    padding: Constants.Sizes.fridgeItemPadding,
+                    child: Icon(Icons.edit),
+                  ),
+                ],
+              ),
+              TableRow(
+                children: <Widget>[
+                  Widgets.H2(style, "Pizzadeeg"),
+                  Widgets.H2(style, "230 gram"),
+                  Padding(
+                    padding: Constants.Sizes.fridgeItemPadding,
+                    child: Icon(Icons.edit),
+                  ),
+                ],
+              ),
+              TableRow(
+                children: <Widget>[
+                  Widgets.H2(style, "Olijf-olie"),
+                  Widgets.H2(style, "200 milliliter"),
+                  Padding(
+                    padding: Constants.Sizes.fridgeItemPadding,
+                    child: Icon(Icons.edit),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
