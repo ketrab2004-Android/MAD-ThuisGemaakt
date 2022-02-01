d5b009463e9c20fb4b4d443283da37984ccd218f
Bartek O <bartek@fos.nl>
2022-02-01 10:45:08 +0100

    Made use of page transition package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:thuis_gemaakt/pages/l.dart' as Pages;
 import 'package:thuis_gemaakt/styles/l.dart' as Styles;
 import 'package:thuis_gemaakt/constants/l.dart' as Constants;
@@ -13,12 +14,51 @@ void main() {
     ),
 
     initialRoute: '/',
-    routes: <String, WidgetBuilder>{
-      '/': (ctx) => Pages.Home(title: Constants.Text.appTitle, style: style),
-      '/favourites': (ctx) => Pages.Favourites(title: Constants.Text.appTitle, style: style),
-      '/explore': (ctx) => Pages.Explore(title: Constants.Text.appTitle, style: style),
-      '/account': (ctx) => Pages.Account(title: Constants.Text.appTitle, style: style),
-      '/fridge': (ctx) => Pages.Fridge(title: Constants.Text.appTitle, style: style),
+    // routes: <String, WidgetBuilder>{
+    //   '/': (ctx) => Pages.Home(title: Constants.Text.appTitle, style: style),
+    //   '/favourites': (ctx) => Pages.Favourites(title: Constants.Text.appTitle, style: style),
+    //   '/explore': (ctx) => Pages.Explore(title: Constants.Text.appTitle, style: style),
+    //   '/account': (ctx) => Pages.Account(title: Constants.Text.appTitle, style: style),
+    //   '/fridge': (ctx) => Pages.Fridge(title: Constants.Text.appTitle, style: style),
+    // },
+    onGenerateRoute: (settings) {
+      switch (settings.name) {
+        case '/favourites':
+          return PageTransition(
+            child: Pages.Favourites(title: Constants.Text.appTitle, style: style),
+            type: PageTransitionType.fade,
+            settings: settings,
+          );
+
+        case '/explore':
+          return PageTransition(
+            child: Pages.Explore(title: Constants.Text.appTitle, style: style),
+            type: PageTransitionType.fade,
+            settings: settings,
+          );
+
+        case '/account':
+          return PageTransition(
+            child: Pages.Account(title: Constants.Text.appTitle, style: style),
+            type: PageTransitionType.fade,
+            settings: settings,
+          );
+
+        case '/fridge':
+          return PageTransition(
+            child: Pages.Fridge(title: Constants.Text.appTitle, style: style),
+            type: PageTransitionType.fade,
+            settings: settings,
+          );
+
+        case '/':
+        default:
+          return PageTransition(
+            child: Pages.Home(title: Constants.Text.appTitle, style: style),
+            type: PageTransitionType.fade,
+            settings: settings,
+          );
+      }
     },
   ));
 }
