08b7d376072bd4a48621051e09defa13c9e287f6
Bartek O <bartek@fos.nl>
2022-02-01 10:32:20 +0100

    Refactored name of app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:thuis_gemaakt/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ThuisGemaakt());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
