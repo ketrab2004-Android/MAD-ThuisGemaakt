b933033904e39653b436bcaf5f42496c4cdc4678
Bartek O <bartek@fos.nl>
2022-02-01 16:34:44 +0100

    Added http dependencies for pages library

--- lib/pages/l.dart
+++ lib/pages/l.dart
@@ -1,9 +1,12 @@
 library pages;
 
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:thuis_gemaakt/widgets/l.dart' as Widgets;
 import 'package:thuis_gemaakt/constants/l.dart' as Constants;
 import 'package:thuis_gemaakt/styles/l.dart' as Styles;
+import 'package:thuis_gemaakt/data/l.dart' as Data;
 
 part 'Home.dart';
 part 'Favourites.dart';
