e8dcda69cbb33014329cc9310475773a17077bc8
Bartek O <bartek@fos.nl>
2022-02-01 10:32:08 +0100

    Added appBase to widgets library

--- lib/widgets/l.dart
+++ lib/widgets/l.dart
@@ -2,5 +2,7 @@ library widgets;
 
 import 'package:flutter/material.dart';
 import 'package:thuis_gemaakt/styles/l.dart' as Styles;
+import 'package:thuis_gemaakt/constants/l.dart' as Constants;
 
+part 'AppBase.dart';
 part 'H1.dart';
