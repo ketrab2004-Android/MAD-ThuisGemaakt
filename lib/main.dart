90149dc5f52523cffb1ded0654918e3859dd474f
Bartek O <bartek@fos.nl>
2022-02-01 10:33:23 +0100

    Removed statelessWidget for a materialApp with routes

--- lib/main.dart
+++ lib/main.dart
@@ -4,55 +4,21 @@ import 'package:thuis_gemaakt/styles/l.dart' as Styles;
 import 'package:thuis_gemaakt/constants/l.dart' as Constants;
 
 void main() {
-  runApp(MyApp());
-}
+  Styles.Base style = Styles.Lightmode();
 
-class MyApp extends StatelessWidget {
-  Styles.Base theme = Styles.Lightmode();
+  runApp(MaterialApp(
+    title: Constants.Text.appTitle,
+    theme: ThemeData(
+      primarySwatch: style.themeSwatch,
+    ),
 
-  List< List > navigationItems = [
-    [Constants.Text.thisWeekName, Icons.home],
-    [Constants.Text.favouritesName, Icons.favorite],
-    [Constants.Text.exploreName, Icons.search],
-    [Constants.Text.accountName, Icons.person],
-    [Constants.Text.fridgeName, Icons.kitchen],
-  ];
-
-  // if (true) {
-  //   navigationItems.add(
-  //     [Constants.Text.fridgeName, Icons.fridge]
-  //   );
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: Constants.Text.appTitle,
-      theme: ThemeData(
-        primarySwatch: theme.themeSwatch,
-      ),
-      home: Scaffold(
-        body: SafeArea(
-          child: Pages.HomePage(
-            style: theme,
-            title: Constants.Text.appTitle,
-          ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: new List.generate(navigationItems.length, (index) {
-            List item = navigationItems[index];
-            return new BottomNavigationBarItem(
-              icon: Icon(
-                item[1],
-                color: theme.navigationIconColor,
-              ),
-              label: item[0],
-            );
-          }),
-          showUnselectedLabels: true,
-          selectedItemColor: theme.navigationIconSelectedColor,
-        ),
-      ),
-    );
-  }
+    initialRoute: '/',
+    routes: <String, WidgetBuilder>{
+      '/': (ctx) => Pages.HomePage(title: Constants.Text.appTitle, style: style),
+      '/favourites': (ctx) => Pages.Favourites(title: Constants.Text.appTitle, style: style),
+      '/explore': (ctx) => Pages.Explore(title: Constants.Text.appTitle, style: style),
+      '/account': (ctx) => Pages.Account(title: Constants.Text.appTitle, style: style),
+      '/fridge': (ctx) => Pages.Fridge(title: Constants.Text.appTitle, style: style),
+    },
+  ));
 }
