4c3d4f689c636ce662733dfdfdb3a82e889dbfe9
Bartek O <bartek@fos.nl>
2022-02-01 20:53:50 +0100

    Added size for checkmarks

--- lib/constants/Sizes.dart
+++ lib/constants/Sizes.dart
@@ -5,6 +5,10 @@ class Sizes {
 
   static const EdgeInsetsGeometry weekTitlePadding = EdgeInsets.fromLTRB(2.5, 5.0, 2.5, 10.0);
   static const EdgeInsetsGeometry weekItemsPadding = EdgeInsets.all(10.0);
+
   static const double weekVSpacing = 10.0;
   static const double weekHSpacing = 20.0;
+
+  static const double checkIconSize = 50.0;
+  static const double checkIconBorderSize = 55.0;
 }
