1e578b90c317df224a9f60a35fe4491ad180a87b
Bartek O <bartek@fos.nl>
2022-02-01 10:32:29 +0100

    Add text styles

--- lib/styles/Base.dart
+++ lib/styles/Base.dart
@@ -4,11 +4,25 @@ abstract class Base {
   MaterialColor themeSwatch = Colors.red;
   Color? get theme { return this.themeSwatch[500]; }
 
+  TextStyle h1 = new TextStyle(
+    color: Colors.black,
+    fontSize: 20.0,
+  );
+
   Color navigationIconColor = Colors.black;
-  Color navigationIconSelectedColor = Colors.black12;
+  Color navigationIconSelectedColor = Colors.black45;
+
+  TextStyle navigationText = new TextStyle(
+    color: Colors.black,
+    fontSize: 12.0,
+  );
+  TextStyle navigationHighlightedText = new TextStyle(
+    color: Colors.black45,
+    fontSize: 12.0,
+  );
 
   Color text = Colors.black;
-  Color disabledText = Colors.black12;
+  Color disabledText = Colors.black26;
 
   Color background = Colors.white;
 }
